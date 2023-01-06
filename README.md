d97aee9edbe5d1c7c63b606ba3edbb79ac10e49c
cofedream <cofedream@users.noreply.github.com>
2023-01-06 07:22:41 +0000

    deploy: masiro-moe/masiro-app-flutter@31ae91ee8897cb2541f3edf7dfd82e2017b0d603

--- README.md
+++
@@ -1,3 +0,0 @@
-# 网站站点
-
-[flutter web](https://masiro-moe.github.io/)
--- flutter/flutter_service_worker.js
+++ flutter/flutter_service_worker.js
@@ -30,8 +30,8 @@ const RESOURCES = {
 "assets/assets/image/system/logo.png": "cbb8e407a4049aa0386d0c03d89cc823",
 "assets/assets/image/system/error.png": "501f691a5a68573d20450b43e4d408bd",
 "assets/NOTICES": "8cc459f4e68a9c89bcd94f356b45edce",
-"index.html": "cb70f109af40fb374ff04149517c99ee",
-"/": "cb70f109af40fb374ff04149517c99ee",
+"index.html": "cb9f720a1340c6b89e62495cdb4bd87f",
+"/": "cb9f720a1340c6b89e62495cdb4bd87f",
 "favicon.webp": "5308586dec554fef51ccfed40f79076e",
 "version.json": "092d0cd4393bccdb08f2fcd7290d6a1f"
 };
